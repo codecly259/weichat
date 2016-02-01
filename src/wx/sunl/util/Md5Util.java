1f92ea7cfe26d10cecb9ca0863468caa57df119b
codecly <maxinchun0215@163.com>
2016-02-02 00:44:02 +0800

    更新翻译异常问题

--- src/wx/sunl/util/Md5Util.java
+++ src/wx/sunl/util/Md5Util.java
@@ -3,16 +3,17 @@ package wx.sunl.util;
 import java.security.MessageDigest;
 
 import org.apache.axis.types.HexBinary;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Md5Util {
 	public static final Logger logger = LoggerFactory.getLogger(Md5Util.class);
-	
+
 	/**
 	 * md5加密
-	 * @param data 需要加密的字符串
+	 * 
+	 * @param data
+	 *            需要加密的字符串
 	 * @return 加密后的结果字符串
 	 */
 	public static final String encryptionWithMd5(String data) {
@@ -33,9 +34,9 @@ public class Md5Util {
 			return null;
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		String data = "2015063000000001apple143566028812345678"; //f89f9594663708c1605f3d736d01d2d4
+		String data = "2015063000000001apple143566028812345678"; // f89f9594663708c1605f3d736d01d2d4
 		System.out.println(encryptionWithMd5(data));
 	}
 }
